383ea950b310a5a2c7162a72eb9791d94fe2a960
Shadab-k <shadabpp27.com>
2023-09-07 12:01:54 +0530

    add

---
+++ chat-box/src/App.js
@@ -0,0 +1,47 @@
+
+import './App.css';
+// import Form from './componenets/Form';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './componenets/Home/Home';
+import LogIn from './componenets/LogIn/LogIn';
+import SignUp from './componenets/SignUp/SignUp';
+import { useEffect, useState } from 'react';
+import { auth } from './firebase';
+
+
+
+
+function App() {
+
+const[userName,setUserName] = useState("")
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if(user){
+        setUserName(user.displayName)
+      }
+      else{
+        setUserName("")
+      }
+    })
+  }, [])
+  return (
+    <>
+      <div>
+        <Router>
+          <Routes>
+            <Route path='/' element={<Home name={userName}/>} />
+            <Route path='/login' element={<LogIn />} />
+            <Route path='/signup' element={<SignUp />} />
+          </Routes>
+        </Router>
+
+        {/* <Form /> */}
+      </div>
+
+
+    </>
+  );
+}
+
+export default App;
